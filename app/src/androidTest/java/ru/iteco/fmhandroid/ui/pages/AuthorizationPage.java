28752669a9eb63b189e93eb1895c6d10ce53e390
Dmitrij Sokolov <darkfalken88@gmail.com>
2026-06-17 14:50:12 +0300

    Change AuthorizationPage

--- app/src/androidTest/java/ru/iteco/fmhandroid/ui/pages/AuthorizationPage.java
+++ app/src/androidTest/java/ru/iteco/fmhandroid/ui/pages/AuthorizationPage.java
@@ -12,21 +12,28 @@ import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import androidx.test.espresso.ViewInteraction;
+
 import ru.iteco.fmhandroid.R;
+import ru.iteco.fmhandroid.ui.data.TestData;
+import ru.iteco.fmhandroid.ui.utils.ToastMatcher;
 import ru.iteco.fmhandroid.ui.utils.ViewUtils;
 
 public class AuthorizationPage {
 
+    public static final int ENTER_BUTTON_ID = R.id.enter_button;
+    public static final String LOGIN_FIELD_HINT = "Login";
+    public static final String PASSWORD_FIELD_HINT = "Password";
+
     public ViewInteraction getLoginField() {
-        return onView(withHint("Login"));
+        return onView(withHint(LOGIN_FIELD_HINT));
     }
 
     public ViewInteraction getPasswordField() {
-        return onView(withHint("Password"));
+        return onView(withHint(PASSWORD_FIELD_HINT));
     }
 
     public ViewInteraction getSignInButton() {
-        return onView(withId(R.id.enter_button));
+        return onView(withId(ENTER_BUTTON_ID));
     }
 
     public void login(String login, String password) {
@@ -34,23 +41,29 @@ public class AuthorizationPage {
         getPasswordField().perform(clearText(), typeText(password), closeSoftKeyboard());
         getSignInButton().perform(click());
     }
+
     public void loginEmpty() {
         getLoginField().perform(clearText(), closeSoftKeyboard());
         getPasswordField().perform(clearText(), closeSoftKeyboard());
         getSignInButton().perform(click());
     }
 
-
-    public void checkErrorMessage(String expectedText) {
-        ViewUtils.waitForView(withText(expectedText), 5000);
-        onView(withText(expectedText)).check(matches(isDisplayed()));
+    public void waitForPageLoaded() {
+        ViewUtils.waitForView(ENTER_BUTTON_ID, 15000);
+        onView(withId(ENTER_BUTTON_ID)).check(matches(isDisplayed()));
     }
 
-    public void waitForAuthorizationScreen() {
-        ViewUtils.waitForView(R.id.enter_button, 15000);
+    public void checkWrongLoginAndPasswordError() {
+        onView(withText(TestData.WRONG_LOGIN_AND_PASSWORD_ERROR))
+                .inRoot(new ToastMatcher())
+                .check(matches(isDisplayed()));
+        onView(withId(ENTER_BUTTON_ID)).check(matches(isDisplayed()));
     }
 
-    public boolean isAuthorizationScreenShown() {
-        return ViewUtils.isViewDisplayed(R.id.enter_button);
+    public void checkEmptyFieldsError() {
+        onView(withText(TestData.EMPTY_FIELDS_ERROR))
+                .inRoot(new ToastMatcher())
+                .check(matches(isDisplayed()));
+        onView(withId(ENTER_BUTTON_ID)).check(matches(isDisplayed()));
     }
 }
