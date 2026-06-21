ae4df593e1c3bcb5584b5e984a3682e2bf6e4705
Dmitrij Sokolov <darkfalken88@gmail.com>
2026-06-21 14:32:02 +0300

    Change TestData

--- app/src/androidTest/java/ru/iteco/fmhandroid/ui/data/TestData.java
+++ app/src/androidTest/java/ru/iteco/fmhandroid/ui/data/TestData.java
@@ -5,9 +5,20 @@ public class TestData {
     public static final String VALID_PASSWORD = "password2";
     public static final String WRONG_PASSWORD = "wrong";
     public static final String WRONG_LOGIN = "wrongLogin";
-    //public static final String LONG_TITLE = "A".repeat(201);
-    public static final String HTML_TAG = "<script>alert(1)</script>";
+
     public static final String EMPTY_FIELDS_ERROR = "Login and password cannot be empty";
     public static final String WRONG_LOGIN_AND_PASSWORD_ERROR = "Wrong login or password";
+
+    public static final String NEWS_MENU_ITEM = "News";
+    public static final String ABOUT_MENU_ITEM = "About";
+    public static final String LOGOUT_MENU_ITEM = "Log out";
+
+    public static final String SAVE_BUTTON_TEXT = "Save";
+    public static final String CANCEL_BUTTON_TEXT = "Cancel";
+    public static final String OK_BUTTON_TEXT = "OK";
+    public static final String FILTER_BUTTON_TEXT = "Filter";
+
+    public static final String CONFIRM_DELETE_MESSAGE = "Are you sure you want to permanently delete the document? These changes cannot be reversed in the future.";
+    public static final String CONFIRM_CANCEL_MESSAGE = "The changes won't be saved, do you really want to cancel?";
 
 }
