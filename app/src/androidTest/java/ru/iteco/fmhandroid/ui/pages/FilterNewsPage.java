135020d22c4a3b4282e4b6eead2bad80a21a63d5
Dmitrij Sokolov <darkfalken88@gmail.com>
2026-06-21 14:35:44 +0300

    Change FilterNewsPage

--- app/src/androidTest/java/ru/iteco/fmhandroid/ui/pages/FilterNewsPage.java
+++ app/src/androidTest/java/ru/iteco/fmhandroid/ui/pages/FilterNewsPage.java
@@ -16,38 +16,46 @@ import ru.iteco.fmhandroid.ui.utils.ViewUtils;
 
 public class FilterNewsPage {
 
+    public static final int FILTER_ICON_ID = R.id.filter_news_material_button;
+    public static final int APPLY_FILTER_BUTTON_ID = R.id.filter_button;
+    public static final int CATEGORY_AUTO_COMPLETE_TEXT_VIEW = R.id.news_item_category_text_auto_complete_text_view;
+    public static final int START_DATE_EDIT_TEXT = R.id.news_item_publish_date_start_text_input_edit_text;
+    public static final int END_DATE_EDIT_TEXT = R.id.news_item_publish_date_end_text_input_edit_text;
+
     public void waitForPageLoaded() {
-        ViewUtils.waitForView(R.id.filter_button, 10000);
+        ViewUtils.waitForView(APPLY_FILTER_BUTTON_ID, 10000);
     }
 
     public void checkPageDisplayed() {
         waitForPageLoaded();
-        onView(withId(R.id.filter_news_material_button)).check(matches(isDisplayed()));
+        onView(withId(APPLY_FILTER_BUTTON_ID)).check(matches(isDisplayed()));
     }
 
     public void selectCategory(String category) {
-        onView(withId(R.id.news_item_category_text_auto_complete_text_view)).perform(click());
+        onView(withId(CATEGORY_AUTO_COMPLETE_TEXT_VIEW)).perform(click());
         onView(withText(category)).inRoot(isPlatformPopup()).perform(click());
     }
 
+    public void clearCategory() {
+        onView(withId(CATEGORY_AUTO_COMPLETE_TEXT_VIEW))
+                .perform(clearText(), closeSoftKeyboard());
+    }
+
     public void setStartDate(String date) {
-        onView(withId(R.id.news_item_publish_date_start_text_input_edit_text))
+        onView(withId(START_DATE_EDIT_TEXT))
                 .perform(replaceText(date), closeSoftKeyboard());
     }
 
     public void setEndDate(String date) {
-        onView(withId(R.id.news_item_publish_date_end_text_input_edit_text))
+        onView(withId(END_DATE_EDIT_TEXT))
                 .perform(replaceText(date), closeSoftKeyboard());
     }
 
-    public void clickFilter() {
-        onView(withId(R.id.filter_news_material_button)).perform(click());
+    public void openFilterScreen() {
+        onView(withId(FILTER_ICON_ID)).perform(click());
     }
-    public void clickFilterButton() {
-        onView(withId(R.id.filter_button)).perform(click());
-    }
-    public void clearCategory() {
-        onView(withId(R.id.news_item_category_text_auto_complete_text_view))
-                .perform(clearText(), closeSoftKeyboard());
+
+    public void applyFilter() {
+        onView(withId(APPLY_FILTER_BUTTON_ID)).perform(click());
     }
 }
