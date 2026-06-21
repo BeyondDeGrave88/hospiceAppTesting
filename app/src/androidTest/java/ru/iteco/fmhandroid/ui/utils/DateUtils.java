a7af469467ad18a3c7dba990766c8b187edec29b
Dmitrij Sokolov <darkfalken88@gmail.com>
2026-06-21 12:51:50 +0300

    Add DateUtils

---
+++ app/src/androidTest/java/ru/iteco/fmhandroid/ui/utils/DateUtils.java
@@ -0,0 +1,21 @@
+package ru.iteco.fmhandroid.ui.utils;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class DateUtils {
+
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+    public static String getToday() {
+        return LocalDate.now().format(FORMATTER);
+    }
+
+    public static String getTomorrow() {
+        return LocalDate.now().plusDays(1).format(FORMATTER);
+    }
+
+    public static String getDateDaysAgo(int days) {
+        return LocalDate.now().minusDays(days).format(FORMATTER);
+    }
+}
