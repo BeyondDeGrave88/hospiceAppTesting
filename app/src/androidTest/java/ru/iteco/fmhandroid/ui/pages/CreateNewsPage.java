8780396f7d2e096f7153bc1a24ea2e321636782e
Dmitrij Sokolov <darkfalken88@gmail.com>
2026-06-21 14:35:10 +0300

    Change CreateNewsPage

--- app/src/androidTest/java/ru/iteco/fmhandroid/ui/pages/CreateNewsPage.java
+++ app/src/androidTest/java/ru/iteco/fmhandroid/ui/pages/CreateNewsPage.java
@@ -3,66 +3,76 @@ package ru.iteco.fmhandroid.ui.pages;
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.isPlatformPopup;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static androidx.test.espresso.matcher.RootMatchers.isPlatformPopup;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
+
 import ru.iteco.fmhandroid.R;
 import ru.iteco.fmhandroid.ui.utils.ViewUtils;
 
 public class CreateNewsPage {
 
+    public static final int CATEGORY_AUTO_COMPLETE_TEXT_VIEW = R.id.news_item_category_text_auto_complete_text_view;
+    public static final int TITLE_EDIT_TEXT = R.id.news_item_title_text_input_edit_text;
+    public static final int PUBLICATION_DATE_EDIT_TEXT = R.id.news_item_publish_date_text_input_edit_text;
+    public static final int PUBLICATION_TIME_EDIT_TEXT = R.id.news_item_publish_time_text_input_edit_text;
+    public static final int DESCRIPTION_EDIT_TEXT = R.id.news_item_description_text_input_edit_text;
+    public static final int SAVE_BUTTON_ID = R.id.save_button;
+    public static final int CANCEL_BUTTON_ID = R.id.cancel_button;
+
     public void waitForPageLoaded() {
-        ViewUtils.waitForView(R.id.news_item_title_text_input_edit_text, 10000);
+        ViewUtils.waitForView(TITLE_EDIT_TEXT, 10000);
     }
 
     public void checkPageDisplayed() {
         waitForPageLoaded();
-        onView(withId(R.id.news_item_title_text_input_edit_text)).check(matches(isDisplayed()));
+        onView(withId(TITLE_EDIT_TEXT)).check(matches(isDisplayed()));
     }
 
     public void selectCategory(String category) {
-        onView(withId(R.id.news_item_category_text_auto_complete_text_view)).perform(click());
+        onView(withId(CATEGORY_AUTO_COMPLETE_TEXT_VIEW)).perform(click());
         onData(allOf(is(instanceOf(String.class)), is(category)))
                 .inRoot(isPlatformPopup())
                 .perform(click());
     }
 
     public void enterTitle(String title) {
-        onView(withId(R.id.news_item_title_text_input_edit_text))
+        onView(withId(TITLE_EDIT_TEXT))
                 .perform(replaceText(title), closeSoftKeyboard());
     }
 
     public void enterPublicationDate() {
-        onView(withId(R.id.news_item_publish_date_text_input_edit_text)).perform(click());
+        onView(withId(PUBLICATION_DATE_EDIT_TEXT)).perform(click());
         onView(withId(android.R.id.button1)).perform(click());
     }
+
     public void enterPublicationDate(String date) {
-        onView(withId(R.id.news_item_publish_date_text_input_edit_text))
+        onView(withId(PUBLICATION_DATE_EDIT_TEXT))
                 .perform(replaceText(date), closeSoftKeyboard());
     }
 
     public void enterTime() {
-        onView(withId(R.id.news_item_publish_time_text_input_edit_text)).perform(click());
+        onView(withId(PUBLICATION_TIME_EDIT_TEXT)).perform(click());
         onView(withId(android.R.id.button1)).perform(click());
     }
 
     public void enterDescription(String description) {
-        onView(withId(R.id.news_item_description_text_input_edit_text)).perform(replaceText(description));
+        onView(withId(DESCRIPTION_EDIT_TEXT)).perform(replaceText(description));
     }
 
     public void clickSave() {
-        onView(withId(R.id.save_button)).perform(scrollTo(), click());
+        onView(withId(SAVE_BUTTON_ID)).perform(scrollTo(), click());
     }
 
     public void clickCancel() {
-        onView(withId(R.id.cancel_button)).perform(scrollTo(), click());
+        onView(withId(CANCEL_BUTTON_ID)).perform(scrollTo(), click());
     }
 }
