ba40dd8415cd298dbfc536a64c159d6cb73d9db1
Dmitrij Sokolov <darkfalken88@gmail.com>
2026-06-24 14:52:26 +0300

    Change AboutPage

--- app/src/androidTest/java/ru/iteco/fmhandroid/ui/pages/AboutPage.java
+++ app/src/androidTest/java/ru/iteco/fmhandroid/ui/pages/AboutPage.java
@@ -1,6 +1,7 @@
 package ru.iteco.fmhandroid.ui.pages;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
@@ -11,6 +12,8 @@ import ru.iteco.fmhandroid.ui.utils.ViewUtils;
 public class AboutPage {
 
     public static final int VERSION_TEXT_VIEW_ID = R.id.about_version_title_text_view;
+    public static final int PRIVACY_POLICY_LINK_ID = R.id.about_privacy_policy_value_text_view;
+    public static final int TERMS_OF_USE_LINK_ID = R.id.about_terms_of_use_value_text_view;
 
     public void waitForPageLoaded() {
         ViewUtils.waitForView(VERSION_TEXT_VIEW_ID, 10000);
@@ -19,5 +22,20 @@ public class AboutPage {
     public void checkPageDisplayed() {
         waitForPageLoaded();
         onView(withId(VERSION_TEXT_VIEW_ID)).check(matches(isDisplayed()));
+    }
+    public void checkPrivacyPolicyLinkDisplayed() {
+        onView(withId(PRIVACY_POLICY_LINK_ID)).check(matches(isDisplayed()));
+    }
+
+    public void checkTermsOfUseLinkDisplayed() {
+        onView(withId(TERMS_OF_USE_LINK_ID)).check(matches(isDisplayed()));
+    }
+
+    public void clickPrivacyPolicyLink() {
+        onView(withId(PRIVACY_POLICY_LINK_ID)).perform(click());
+    }
+
+    public void clickTermsOfUseLink() {
+        onView(withId(TERMS_OF_USE_LINK_ID)).perform(click());
     }
 }
