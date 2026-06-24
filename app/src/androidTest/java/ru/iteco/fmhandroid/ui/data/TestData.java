fa62a63f17a418bf3f6eba0a8d593c2371d903cc
Dmitrij Sokolov <darkfalken88@gmail.com>
2026-06-24 14:52:59 +0300

    Change TestData

--- app/src/androidTest/java/ru/iteco/fmhandroid/ui/data/TestData.java
+++ app/src/androidTest/java/ru/iteco/fmhandroid/ui/data/TestData.java
@@ -13,14 +13,18 @@ public class TestData {
     public static final String ABOUT_MENU_ITEM = "About";
     public static final String LOGOUT_MENU_ITEM = "Log out";
 
-    public static final String SAVE_BUTTON_TEXT = "Save";
     public static final String CANCEL_BUTTON_TEXT = "Cancel";
     public static final String OK_BUTTON_TEXT = "OK";
-    public static final String FILTER_BUTTON_TEXT = "Filter";
 
     public static final String CONFIRM_DELETE_MESSAGE = "Are you sure you want to permanently delete the document? These changes cannot be reversed in the future.";
     public static final String CONFIRM_CANCEL_MESSAGE = "The changes won't be saved, do you really want to cancel?";
+    public static final String FILL_EMPTY_FIELDS_ERROR = "Fill empty fields";
+    public static final String SAVING_FAILED_ERROR = "Saving failed. Try again later";
+    public static final String XSS_PAYLOAD = "<script>alert(1)</script>";
 
     public static final String QUOTE_TITLE = "\"Хоспис для меня - это то, каким должен быть мир.\"";
     public static final String QUOTE_DESCRIPTION = "\"Ну, идеальное устройство мира в моих глазах. Где никто не оценивает, никто не осудит, где говоришь, и тебя слышат, где, если страшно, тебя обнимут и возьмут за руку, а если холодно тебя согреют.\" Юля Капис, волонтер";
+
+    public static final String PRIVACY_POLICY_URL = "https://vhospice.org/#/privacy-policy/";
+    public static final String TERMS_OF_USE_URL = "https://vhospice.org/#/terms-of-use";
 }
